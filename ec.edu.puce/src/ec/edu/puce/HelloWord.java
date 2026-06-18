628755960729c2385ffb0dd0645cae26f11d853d
danieldafaf <144976261+danieldafaf@users.noreply.github.com>
2026-06-17 22:01:00 -0500

    Add files via upload

---
+++ ec.edu.puce/src/ec/edu/puce/HelloWord.java
@@ -0,0 +1,34 @@
+package ec.edu.puce;
+
+public class HelloWord {
+
+	public static void main (String[] args) {
+
+	System.out.println ("Hello Word");
+
+	IO.println("Alternativa a partir de la version 25");
+
+	int edad = 20;
+
+	boolean mayorEdad = false;
+
+	String nombre = "Daniel";
+
+	System.out.println("La edad es:" + edad + "y su nombre es " + nombre); 
+	System.out.printf("La edad es :%s y su nombre es %s\n", edad, nombre);
+
+	if (edad > 18) {
+
+	System.out.println("\tMayor de edad");
+
+	}
+
+
+	else {
+
+
+	System.out.println("\tMayor de edad");
+	}
+	}
+}
+
